aa52a126c7d23ac03bb4597ec352076c9c31be06
Elena <kahatanna@gmail.com>
2022-07-22 03:08:28 +0300

    Update Task01 - deleted some comments code

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -57,51 +57,6 @@ int[][] ChangeMatrix(int[][] matrix)
     return matrix;
 }
 
-// int[] SortArray(int[] arr)
-// {
-//     int min = arr[0];
-//     int count = arr.Length;
-//     int[] result = new int[count];
-
-//     if (count < 2) return arr;
-//     else
-//     {
-//         int pivot = arr[0];
-//         int[] less;
-//         int[] greater;
-
-//         for (int i = 0; i < arr.Length; i++)
-//         {
-//             if (arr[i] >= pivot)
-//             { 
-//                 Array.Resize<int>(greater, greater.Length+1);
-//                 greater[greater.Length-1] = arr[i];
-//             } 
-//             else
-//             {
-//                 Array.Resize<int>(less, less.Length+1);
-//                 less[less.Length-1] = arr[i];
-//             }
-//         }
-
-
-
-//         result = Unify(SortArray(less), pivot,  SortArray(greater));
-//         return result;
-//     }
-// }
-
-// int[] Unify (int[] a, int p, int[] b)
-// {
-//    int[] c = new int[a.Length+b.Length + 1];
-//    for(int i = 0; i < a.Length; i++)
-//       c[i] = a[i];
-//     c[a.Length+1] = p;
-//    for(int j = 0; j < b.Length; j++)
-//       c[a.Length + j + 1] = b[j];
-//    return c;
-// }
-
 
 Console.WriteLine("Введите число m:");
 int m = int.Parse(Console.ReadLine());
